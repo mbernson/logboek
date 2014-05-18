fa1ace7e3403175a89f05d785e86edd4a1578305
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-05-18 10:50:34 +0200

    Use jQuery 2.1.1

--- app/views/layouts/application.blade.php
+++ app/views/layouts/application.blade.php
@@ -112,7 +112,7 @@
 </div><!--/.container-->
 
 <!-- Bootstrap core JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/offcanvas.js"></script>
 <script src="/js/entry.js"></script>
