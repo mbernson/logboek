08658416d382b4dd1faf7860fdfca7e538453183
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-05-19 12:05:55 +0200

    Fix migration name

--- app/database/migrations/2014_05_18_110439_add_soft_deletion_to_entries_and_tasks.php
+++ app/database/migrations/2014_05_18_110439_add_soft_deletion_to_entries_and_tasks.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSoftDeletionToTasksAndEntries extends Migration {
+class AddSoftDeletionToEntriesAndTasks extends Migration {
 
 	/**
 	 * Run the migrations.
