0bb43540ac1720d65cf1f7aeb51e9882ee947243
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-05-31 11:40:39 +0200

    Add user show view

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-<h1><em>TODO</em></h1>
+<div class="form-group">
+	{{ Form::label('username', 'Gebruikersnaam') }}
+	{{ Form::text('username', $user->username, ['class' => 'form-control', 'disabled' => 1]) }}
+</div>
+
+<div class="form-group">
+	{{ Form::label('email', 'E-mail adres') }}
+	{{ Form::text('email', $user->email, ['class' => 'form-control', 'disabled' => 1]) }}
+</div>
 
 @stop
