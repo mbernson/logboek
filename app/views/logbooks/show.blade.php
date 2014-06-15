68018d7077d78235f791a736ac2c0aa08fbab367
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-06-15 12:08:37 +0200

    Update logbooks.show view

--- app/views/logbooks/show.blade.php
+++ app/views/logbooks/show.blade.php
@@ -5,17 +5,15 @@
 <h1>{{{ $logbook->title }}}</h1>
 
 <p>
-
-@if ($logbook->user_id == (Auth::user()->id) || $logbook->user_id == 0)
-	<a class="btn btn-primary btn-lg" href="{{ action('logbooks.entries.create', [$logbook->id]) }}">Nieuwe entry</a>
-@else
-	<a class="btn btn-primary btn-lg" disabled="disabled">Nieuwe entry</a>
-@endif
-
+	@if ($logbook->user_id == (Auth::user()->id) || $logbook->user_id == 0)
+		<a class="btn btn-primary btn-lg" href="{{ action('logbooks.entries.create', [$logbook->id]) }}">Nieuwe entry</a>
+	@else
+		<a class="btn btn-primary btn-lg" disabled="disabled">Nieuwe entry</a>
+	@endif
 </p>
 
 @if($logbook->entries->count() == 0)
-        <p>Er zijn geen logboek entry's gevonden voor het logboek '<b>{{ $logbook->title }}</b>'!</p>
+	<p>Er zijn geen logboek entries gevonden voor het logboek <strong>{{ $logbook->title }}</strong>!</p>
 @endif
 
 @foreach($entries as $entry)
