bfaa6db0d198f9f5138e8bc34a97428e87226e36
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-06-16 09:20:09 +0200

    Update markdown template

--- app/views/markdown/report.blade.php
+++ app/views/markdown/report.blade.php
@@ -1,9 +1,30 @@
-# {{ $title }}
+% {{ $title."\n" }}
+% Projectgroep 2, INF1G
+
+# {{ $title."\n" }}
 
 ## Project forensisch onderzoek
 
-@foreach($users as $user)
-{{{ $user->username || 'Niemand' }}}
+@foreach($logbooks as $logbook)
+
+# {{{ $logbook->title }}}
+
+Eigenaar: {{{ $logbook->owner->first_name }}} {{{ $logbook->owner->last_name }}}
+
+@foreach($logbook->entries as $entry)
+
+# {{{ $entry->title }}}
+
+
+Begonnen op: {{ $entry->started_at }}
+
+
+Geëindigd op: {{ $entry->finished_at }}
+
+
+{{ $entry->body }}	
+
+--------------------------------
 @endforeach
 
-{{ $logbooks->first()->title }}
+@endforeach
