c845042fd0fbd2c27acf2f05f6a46d2ac6101691
Bart <l0ngestever@github>
2015-02-18 15:37:44 +0100

    Adding legals rows (seed)

--- app/database/seeds/LegalsTableSeeder.php
+++ app/database/seeds/LegalsTableSeeder.php
@@ -14,6 +14,7 @@ class LegalsTableSeeder extends Seeder {
 
     if($exist == 0) {
       $legals = array(
+        /* Forensische Artikel 1 - COMPUTERVREDEBREUK */
         array(
           'name' => 'Computervredebreuk',
           'body' => '',
@@ -21,12 +22,50 @@ class LegalsTableSeeder extends Seeder {
           'abbreviation' => 'Artikel 138ab',
           'code' => 'Wetboek van strafrecht',
         ),
+
+        /* Forensische Artikel 2 - RECHTEN VAN DE MENS (PRIVACY) */
         array(
-          'name' => 'biuer',
+          'name' => 'Rechten van de mens - Privacy',
           'body' => '',
-          'html_body' => 'test',
-          'abbreviation' => 'hai',
-          'code' => 'Wetboek van strafvordering',
+          'html_body' => '',
+          'abbreviation' => 'Artikel 8',
+          'code' => 'Europees Verdrag - RVDM',
+        ),
+
+        /* Forensische Artikel 3 - INBESLAGNEMING (ALGEMEEN) */
+        array(
+          'name' => 'Inbeslagneming (algemeen)',
+          'body' => '',
+          'html_body' => '',
+          'abbreviation' => 'Artikel 94',
+          'code' => 'Wetboek van Strafvordering',
+        ),
+
+        /* Forensische Artikel 4 INBESLAGNEMING (OPSPORINGSAMBTENAREN OF BIJZONDERE PERSONEN) */
+        array(
+          'name' => 'Inbeslagneming (opsporingsambtenaren of bijzondere personen)',
+          'body' => '',
+          'html_body' => '',
+          'abbreviation' => 'Artikel 95',
+          'code' => 'Wetboek van Strafvordering',
+        ),
+
+        /* Forensische Artikel 5 VORDERING (COMMUNICATIE TAB) */
+        array(
+          'name' => 'Vordering (communicatie tab)',
+          'body' => '',
+          'html_body' => '',
+          'abbreviation' => 'Artikel 126n',
+          'code' => 'Wetboek van Strafvordering',
+        ),
+
+        /* Forensische Artikel 6 VORDERING (COMMUNICATIE GEGEVENS)*/
+        array(
+          'name' => 'Vordering (communicatie gegevens)',
+          'body' => '',
+          'html_body' => '',
+          'abbreviation' => 'Artikel 126na',
+          'code' => 'Wetboek van Strafvordering',
         )
       );
 
